8748e2d22aa76da91cd2ac47695d407f2bf27496
da-ahmed <bbarkouti@gmail.com>
2018-04-25 14:57:25 +0100

    ajout des component fix fontawsome 1

---
+++ src/app/lstvehicle/lstvehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lstvehicle',
+  templateUrl: './lstvehicle.component.html',
+  styleUrls: ['./lstvehicle.component.css']
+})
+export class LstvehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
